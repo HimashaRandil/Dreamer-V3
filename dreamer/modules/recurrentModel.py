706b81825834e3fa88bf9d66d53c77aad01a9764
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-11-10 15:00:41 +0530

    initial hidden state created

--- dreamer/modules/recurrentModel.py
+++ dreamer/modules/recurrentModel.py
@@ -4,12 +4,14 @@ import torch.nn.functional as F
 import os
 
 class RecurrentModel(nn.Module):
-    def __init__(self, config):
+    def __init__(self, config, device):
         super(RecurrentModel, self).__init__()
         
         self.latent_dim = config.latent_dim      # Dimension of the latent state z_t
         self.action_dim = config.action_dim      # Dimension of the action space a_t
         self.hidden_dim = config.hidden_dim      # Dimension of the hidden state h_t in the RNN
+        self.config = config
+        self.device = device
         
         # Define the GRU layer
         self.gru = nn.GRUCell(input_size=self.latent_dim + self.action_dim, 
@@ -30,4 +32,8 @@ class RecurrentModel(nn.Module):
         h_t = self.gru(gru_input, h_t_prev)
         
         return h_t 
+    
+
+    def input_init(self, batch_size):
+        return torch.zeros(batch_size, self.config.hidden_dim).to(self.device)
     
